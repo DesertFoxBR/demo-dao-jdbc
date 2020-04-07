6ac122d4142ed3cf9adbd406de8c0c6c060ad090
DesertFoxBR <everton.estevan@outlook.com>
2020-04-07 11:35:39 -0300

    DAO interfaces

--- src/model/entities/Department.java
+++ src/model/entities/Department.java
@@ -5,23 +5,22 @@ import java.io.Serializable;
 public class Department implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
-	private int id;
+	private Integer id;
 	private String name;
 	
-	public Department () {
-		
+	public Department () {	
 	}
 
-	public Department(int id, String name) {
+	public Department(Integer id, String name) {
 		this.id = id;
 		this.name = name;
 	}
 
-	public int getId() {
+	public Integer getId() {
 		return id;
 	}
 
-	public void setId(int id) {
+	public void setId(Integer id) {
 		this.id = id;
 	}
 
@@ -37,7 +36,7 @@ public class Department implements Serializable {
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + id;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
 
@@ -50,7 +49,10 @@ public class Department implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		Department other = (Department) obj;
-		if (id != other.id)
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
 			return false;
 		return true;
 	}
