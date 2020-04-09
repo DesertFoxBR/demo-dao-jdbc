8eed43377f4d1fcfb389605afb78b9edc89870bd
DesertFoxBR <everton.estevan@outlook.com>
2020-04-09 17:22:12 -0300

    Find by department

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,11 @@
 package application;
 
+import java.util.List;
 import java.util.Locale;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
@@ -12,11 +14,14 @@ public class Program {
 		
 		Locale.setDefault(Locale.US);
 		
-		System.out.println("===== Test 1: seller findById =====");
+		System.out.println("===== Test 2: seller findByDepartment =====");
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
-		Seller seller = sellerDao.findById(3);
-		System.out.println(seller);
+		Department dep = new Department(2,null);
+		List<Seller> list = sellerDao.findByDepartment(dep);
+		for (Seller obj : list) {
+			System.out.println(obj);
+		}
 
 	}
 
